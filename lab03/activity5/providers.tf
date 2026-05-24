b4d54ed7aed7409d0b82d159f157b55bf27a226d
Garret Arcoraci <gpavks@rit.edu>
2026-05-24 12:07:36 -0400

    Files needed for Activity 5, Lab 3

---
+++ lab03/activity5/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "c28f39ec-ee24-4bb5-8030-835dd020310d"
+}
