914747b2bdb2276eeb718e8f488ad4d30663e4b2
Garret Arcoraci <gpavks@rit.edu>
2026-05-24 12:09:37 -0400

    File needed for Lab 2

--- lab02/main.tf
+++ lab02/main.tf
@@ -1,16 +1,7 @@
-##############################################
-# main.tf – Deploy an Ubuntu VM on Hyper-V
-# -------------------------------------------
-# This Terraform configuration:
-# 1. Pulls in the community Hyper-V provider.
-# 2. Connects to the local Hyper-V host via WinRM.
-# 3. Defines and powers-off an Ubuntu 24.04 VM
-#    with 2 vCPUs, 2 GiB RAM, an attached ISO,
-#    and default-switch networking.
-##############################################
+# =============================================================================
+# main.tf - Lab 2: Ubuntu 26.04 Desktop on Hyper-V
+# =============================================================================
 
-
-# ---------- 1. Tell Terraform which provider to use ----------
 terraform {
   required_providers {
     hyperv = {
@@ -20,52 +11,43 @@ terraform {
   }
 }
 
-# ---------- 2. Configure the Hyper-V provider ----------
 provider "hyperv" {
   use_ntlm = true
   https    = false
   port     = 5985
 
-  # Hostname or IP of the Windows machine running Hyper-V
-  host = "localhost"
-
-  # Local Windows credentials that have Hyper-V rights
+  host     = "localhost"
   user     = "student"
   password = "student"
 }
 
-# ---------- 3. Declare the virtual machine ----------
+# =============================================================================
 resource "hyperv_machine_instance" "ubuntu" {
   name       = "ubuntu-26-04"
   generation = 2
   state      = "Off"
 
-  # ---------- Hardware ----------
-  memory_startup_bytes = 2147483648
+  # --------------------- Hardware ---------------------
+  memory_startup_bytes = var.memory_size
   processor_count      = 2
   static_memory        = true
 
-  # ---------- Networking ----------
+  # --------------------- Networking ---------------------
   network_adaptors {
     name        = "lan0"
     switch_name = "Default Switch"
   }
 
-  # ---------- Storage: attach Ubuntu ISO as a virtual DVD ----------
+  # --------------------- Storage - Ubuntu ISO ---------------------
   dvd_drives {
     controller_number   = 0
     controller_location = 1
     path                = "C:/isos/ubuntu-26.04-desktop-amd64.iso"
+    resource_pool_name  = "Primordial" # Prevents common apply error
   }
 
-  # ---------- Firmware & boot order ----------
+  # --------------------- Firmware & Boot Order ---------------------
   vm_firmware {
     enable_secure_boot = "Off"
-
-    boot_order {
-      boot_type           = "DvdDrive"
-      controller_number   = 0
-      controller_location = 1
-    }
   }
 }
---
+++ lab02/variables.tf
@@ -0,0 +1,5 @@
+variable "memory_size" {
+  description = "Startup memory in bytes"
+  type = number
+  default = 2147483648
+}
